65ed9948ce46626301bc156ccf411f7c0d6f1fb8
noah-art3mis <costa.araujo.gustavo@gmail.com>
2023-02-28 13:14:04 -0300

    fix: add html error message

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-const button = document.getElementById('my-form');
+const form = document.getElementById('my-form');
+const error_message = document.querySelector(".error-message");
 
-button.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
 	e.preventDefault();
-	let number_a = document.getElementById("number-a");
-	let number_b = document.getElementById("number-b");
+	const number_a = document.getElementById("number-a");
+	const number_b = document.getElementById("number-b");
+
 	if (number_b.value > number_a.value) {
 		alert("B>A!\n\n:)");
 		number_a.value = '';
 		number_b.value = '';
 	}
+})
+
+form.addEventListener('keyup', (e) => {
+	const number_a = document.getElementById("number-a");
+	const number_b = document.getElementById("number-b");
+	if (number_b.value > number_a.value) {
+		error_message.style = '';
+	}
 	else {
-		alert("A>B\n\n:(");
+		error_message.style.display = 'block';
 	}
 })
