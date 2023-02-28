f97d4be9432c50785b1d2945685f7ba31600c93d
noah-art3mis <costa.araujo.gustavo@gmail.com>
2023-02-28 11:56:23 -0300

    refactor: store id in variable instead of value

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ const button = document.getElementById('my-form');
 
 button.addEventListener('submit', (e) => {
 	e.preventDefault();
-	let number_a = document.getElementById("number-a").value;
-	let number_b = document.getElementById("number-b").value;
-	if (number_b === number_a) {
-		alert("A = B\n\n:|");
-	}
-	else if (number_b > number_a) {
+	let number_a = document.getElementById("number-a");
+	let number_b = document.getElementById("number-b");
+	if (number_b.value > number_a.value) {
 		alert("B>A!\n\n:)");
+		number_a.value = '';
+		number_b.value = '';
 	}
 	else {
 		alert("A>B\n\n:(");
