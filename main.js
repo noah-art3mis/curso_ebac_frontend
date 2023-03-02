6d73541ff22c4b80fe4553101f341d75f094cf85
noah-art3mis <costa.araujo.gustavo@gmail.com>
2023-03-02 18:11:48 -0300

    add: jquery validation

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 $(document).ready( () => {
-  $('telefone').mask('(00) 00000-0000');
-  $('cpf').mask('000.000.000-00');
-  $('cep').mask('000.000.000-00');
-}
+  $('#telefone').mask('(00) 00000-0000', {
+	  placeholder: '(61) 12345-6789'
+  });
+  $('#cpf').mask('000.000.000-00');
+  $('#cep').mask('000.000.000-00');
+
+  // n funciona?
+  // $('form').validate({
+  //   rules: {
+  //     nome: {
+  //       required: true
+  //     },
+  //     email: {
+  //       required: true,
+  //       email: true
+  //     },
+  //     telefone: {
+  //       required: true
+  //     },
+  //     cpf: {
+  //       required: true
+  //     },
+  //     endereco: {
+  //       required: true
+  //     },
+  //     cep: {
+  //       required: true
+  //     }
+  //   }
+  // });
+});
