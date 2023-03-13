d50f64aa790398a32dc58551c5e780bb4affe836
noah-art3mis <costa.araujo.gustavo@gmail.com>
2023-03-13 12:16:05 -0300

    consolidate gulp functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
+const imagemin = require('gulp-imagemin');
 
-function compilaSass() {
+function minifyImages() {
+  return gulp
+    .src('./src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'));
+}
+
+function compileSass() {
   return gulp
     .src('./src/styles/main.scss')
     .pipe(sourcemaps.init())
@@ -15,11 +25,28 @@ function compilaSass() {
     .pipe(gulp.dest('./build/styles'));
 }
 
-exports.sass = compilaSass();
-exports.watch = function () {
+function minifyJavascript() {
+  return gulp
+    .src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest('./build/scripts'));
+}
+
+exports.default = function () {
   gulp.watch(
-    '/source/styles/*.scss',
+    './src/images/*',
     { ignoreInitial: false },
-    gulp.series(compilaSass)
+    gulp.series(minifyImages)
+  );
+  gulp.watch(
+    './src/styles/main.scss',
+    { ignoreInitial: false },
+    gulp.series(compileSass)
+  );
+  gulp.watch(
+    './src/scripts/*.js',
+    { ignoreInitial: false },
+    gulp.series(minifyJavascript)
   );
 };
