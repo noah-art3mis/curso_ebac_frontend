18481568ee228223c489c2988630a37deba5d201
noah-art3mis <costa.araujo.gustavo@gmail.com>
2023-07-14 00:36:23 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function multiplica(a, b) {
+    return a * b;
+}
+function saudação(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplica(2, 3));
+console.log(saudação("Fernando"));
---
+++ main.ts
@@ -0,0 +1,10 @@
+function multiplica(a: number, b: number) {
+return a * b
+}
+
+function saudação(nome: string) {
+return "Olá " + nome
+}
+
+console.log(multiplica(2,3))
+console.log(saudação("Fernando"))
